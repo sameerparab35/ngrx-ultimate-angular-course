0a95de36f9e7b982b413a5498928f30a1f3a0549
stano kucharik <kucharik.s@gmail.com>
2017-12-26 11:10:59 +0100

    switch getting pizzas to store

--- src/products/services/pizzas.service.ts
+++ src/products/services/pizzas.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 import { DbService } from './db.service';
 
 import { Pizza } from '../models/pizza.model';
 
+import * as fromReducers from '../store/reducers';
+import * as fromActions from '../store/actions';
+
 @Injectable()
 export class PizzasService {
-    constructor(private dbService: DbService) {}
+    constructor(
+        private dbService: DbService,
+        private store: Store<fromReducers.IProductsState>
+    ) {}
 
     getPizzas() {
-        return this.dbService.getPizzas();
+        this.store.dispatch(new fromActions.LoadPizzas());
+        return this.store.select(fromReducers.selectPizzas);
     }
 
     createPizza(payload: Pizza) {
