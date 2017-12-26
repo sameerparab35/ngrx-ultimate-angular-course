1d8b2c9b610a40ae239fb55622916b172763fba0
stano kucharik <kucharik.s@gmail.com>
2017-12-26 10:52:11 +0100

    formatting and minor refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,36 +12,36 @@ import { storeFreeze } from 'ngrx-store-freeze';
 
 // this would be done dynamically with webpack for builds
 const environment = {
-  development: true,
-  production: false,
+    development: true,
+    production: false
 };
 
 export const metaReducers: MetaReducer<any>[] = !environment.production
-  ? [storeFreeze]
-  : [];
+    ? [storeFreeze]
+    : [];
 
 // bootstrap
 import { AppComponent } from './containers/app/app.component';
 
 // routes
 export const ROUTES: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'products' },
-  {
-    path: 'products',
-    loadChildren: '../products/products.module#ProductsModule',
-  },
+    { path: '', pathMatch: 'full', redirectTo: 'products' },
+    {
+        path: 'products',
+        loadChildren: '../products/products.module#ProductsModule'
+    }
 ];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(ROUTES),
-    StoreModule.forRoot({}, { metaReducers }),
-    EffectsModule.forRoot([]),
-    environment.development ? StoreDevtoolsModule.instrument() : [],
-  ],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot(ROUTES),
+        StoreModule.forRoot({}, { metaReducers }),
+        EffectsModule.forRoot([]),
+        environment.development ? StoreDevtoolsModule.instrument() : []
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 import { Pizza } from '../../models/pizza.model';
 import { PizzasService } from '../../services/pizzas.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-  selector: 'products',
-  styleUrls: ['products.component.scss'],
-  template: `
+    selector: 'products',
+    styleUrls: ['products.component.scss'],
+    template: `
     <div class="products">
       <div class="products__new">
         <a
@@ -16,25 +17,23 @@ import { PizzasService } from '../../services/pizzas.service';
         </a>
       </div>
       <div class="products__list">
-        <div *ngIf="!((pizzas)?.length)">
+        <div *ngIf="!((pizzas$ | async)?.length)">
           No pizzas, add one to get started.
         </div>
         <pizza-item
-          *ngFor="let pizza of (pizzas)"
+          *ngFor="let pizza of (pizzas$ | async)"
           [pizza]="pizza">
         </pizza-item>
       </div>
     </div>
-  `,
+  `
 })
 export class ProductsComponent implements OnInit {
-  pizzas: Pizza[];
+    pizzas$: Observable<Pizza[]>;
 
-  constructor(private pizzaService: PizzasService) {}
+    constructor(private pizzaService: PizzasService) {}
 
-  ngOnInit() {
-    this.pizzaService.getPizzas().subscribe(pizzas => {
-      this.pizzas = pizzas;
-    });
-  }
+    ngOnInit() {
+        this.pizzas$ = this.pizzaService.getPizzas();
+    }
 }
